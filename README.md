1746bde008883d6dd33a96c9ea2bdda51bbfbbe8
lauretteroy <laurieroy.dev@gmail.com>
2021-03-02 23:04:21 -0700

    Add tasks, button, header, task components, react-icons

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Getting Started with Create React App
+# TASK TRACKER
+This app was created following a [tutorial](https://www.youtube.com/watch?v=w7ejDZ8SWv8) to refresh on React
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+		{
+			id: 1,
+			text: 'Get up',
+			day: 'Mar 3d at 8:00am',
+			reminder: false,
+		},
+		{
+			id: 2,
+			text: 'Make Coffee',
+			day: 'Mar 3d at 8:01am',
+			reminder: false,
+		},
+		{
+			id: 3,
+			text: 'Do Yoga',
+			day: 'Mar 3d at 8:05am',
+			reminder: false,
+		},
+		{
+			id: 4,
+			text: 'Code',
+			day: 'Mar 3d at 8:30am',
+			reminder: false,
+		}
+  ])
+  
+  // Delete Task
+  const deleteTask = (id) => {
+    console.log('delete', id)
+  }
+
   return (
     <div className='container'>
-     < Header title='Hello' />
+     < Header />
+     < Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
-  );
+  )
 }
 
 export default App;
